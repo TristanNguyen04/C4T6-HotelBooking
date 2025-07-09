2297ee9215f734f6a62fb4dafcd032abcad469b0
brandonkimchi <brandonkim.184@gmail.com>
2025-07-09 15:34:20 +0800

    home page

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Payment from './pages/PaymentTest';
 import HomePage from './pages/HomePage';
+import Header from "./components/Header";
 import './styles/index.css';
 
 
@@ -8,6 +9,7 @@ import './styles/index.css';
 function App() {
   return (
     <Router>
+      <Header/>
       <Routes>
         <Route path="/payment" element={<Payment />} />
         <Route path="/" element={<HomePage />} />
