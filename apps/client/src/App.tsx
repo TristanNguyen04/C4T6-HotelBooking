85eb4ce2a41a03ec065a94c6cdaab5635e29d935
brandonkimchi <brandonkim.184@gmail.com>
2025-07-17 03:09:44 +0800

    NavBar, Login and Register Pages + Routing

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -9,49 +9,85 @@ import Layout from './layouts/Layout';
 import SearchBar from './components/SearchBar';
 import { useNavigate } from 'react-router-dom';
 
-function App(){
+function App() {
   const navigate = useNavigate();
 
   // Handle SearchBar at Home Page
-  const handleSearchSubmit = ({ destination, checkin, checkout, guests, rooms, adults, children }: {
+  const handleSearchSubmit = ({
+    destination,
+    checkin,
+    checkout,
+    guests,
+    rooms,
+    adults,
+    children,
+  }: {
     destination: { uid: string; term: string };
     checkin: string;
     checkout: string;
     guests: string;
     rooms: number;
     adults: number;
-    children: number
+    children: number;
   }) => {
     navigate(
-      `/search?term=${encodeURIComponent(destination.term)}&destination_id=${destination.uid}&checkin=${checkin}&checkout=${checkout}&guests=${guests}&adults=${adults}&children=${children}&rooms=${rooms}
-    `);
+      `/search?term=${encodeURIComponent(destination.term)}&destination_id=${destination.uid}&checkin=${checkin}&checkout=${checkout}&guests=${guests}&adults=${adults}&children=${children}&rooms=${rooms}`
+    );
   };
 
   return (
     <Routes>
-      {/* Routes without layout */}
-      <Route path='/login' element={<LoginPage/>}/>
-      <Route path='/register' element={<RegisterPage/>}/>
-      <Route path='/verify-email' element={<EmailVerificationPage/>}/>
-      
       {/* Routes with layout */}
-      <Route path='/' element={
-        <Layout showHero={true} heroContent={<SearchBar onSubmit={handleSearchSubmit} />}>
-          <HomePage/>
-        </Layout>
-      }/>
-      <Route path='/search' element={
-        <Layout showHero={false}>
-          <SearchResultsPage/>
-        </Layout>
-      }/>
-      <Route path='/hotels/:id/details' element={
-        <Layout showHero={false}>
-          <HotelDetailsPage/>
-        </Layout>
-      }/>
+      <Route
+        path="/"
+        element={
+          <Layout showHero={true} heroContent={<SearchBar onSubmit={handleSearchSubmit} />}>
+            <HomePage />
+          </Layout>
+        }
+      />
+      <Route
+        path="/search"
+        element={
+          <Layout showHero={false}>
+            <SearchResultsPage />
+          </Layout>
+        }
+      />
+      <Route
+        path="/hotels/:id/details"
+        element={
+          <Layout showHero={false}>
+            <HotelDetailsPage />
+          </Layout>
+        }
+      />
+      <Route
+        path="/login"
+        element={
+          <Layout showHero={false}>
+            <LoginPage />
+          </Layout>
+        }
+      />
+      <Route
+        path="/register"
+        element={
+          <Layout showHero={false}>
+            <RegisterPage />
+          </Layout>
+        }
+      />
+      <Route
+        path="/verify-email"
+        element={
+          <Layout showHero={false}>
+            <EmailVerificationPage />
+          </Layout>
+        }
+      />
     </Routes>
   );
 }
 
-export default App
+export default App;
