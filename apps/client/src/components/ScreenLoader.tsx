26d6985632654014d0571062b67a805bc4f6bf82
brandonkimchi <brandonkim.184@gmail.com>
2025-07-29 20:17:46 +0800

    changed index.html to Ascenda + added words for the loader

--- apps/client/src/components/ScreenLoader.tsx
+++ apps/client/src/components/ScreenLoader.tsx
@@ -1,7 +1,11 @@
 export default function ScreenLoader() {
   return (
-    <div id="loader-wrapper" className="fixed inset-0 bg-white flex items-center justify-center z-[9999]">
+    <div
+      id="loader-wrapper"
+      className="fixed inset-0 bg-white flex flex-col items-center justify-center z-[9999]"
+    >
       <div className="loader"></div>
+      <p className="loading-text font-poppins mt-4">Finding you the best hotels...</p>
     </div>
   );
 }
