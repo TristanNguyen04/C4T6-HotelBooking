08f9abbed4ec297c6a4d629701fad88a28f175f6
USRR <zhaohuing20027@gmail.com>
2025-07-31 15:45:52 +0800

    Added new tests for dbUtil for testing env

--- apps/server/src/controllers/dbController.ts
+++ apps/server/src/controllers/dbController.ts
@@ -2,7 +2,7 @@ import prisma from '../utils/prismaClient';
 import { Request, Response } from 'express';
 
 export const clearBookingTable = async (req: Request, res: Response) => {
-    if (process.env.NODE_ENV !== 'test') {
+    if (process.env.NODE_ENV !== 'test' && process.env.NODE_ENV !== 'jest') {
         return res.status(400).json({ error: 'Access denied. Only allowed in test environment.' });
     }
     try {
@@ -14,7 +14,7 @@ export const clearBookingTable = async (req: Request, res: Response) => {
 }
 
 export const clearUserTable = async (req: Request , res: Response) =>{
-    if(process.env.NODE_ENV !== 'test'){
+    if(process.env.NODE_ENV !== 'test' && process.env.NODE_ENV !== 'jest'){
         return res.status(400).json({error: 'Access denied. Only allowed in test environment.'});
     }
     try{
@@ -22,18 +22,18 @@ export const clearUserTable = async (req: Request , res: Response) =>{
         res.status(200).json({message: 'User table cleared successfully'});
     }
     catch(error){
-        res.status(500).json({error: 'Failed to clear booking table'});
+        res.status(500).json({error: 'Failed to clear user table'});
     }
 }
 
 export const addVerifiedUser = async (req: Request, res: Response) =>{
-    if(process.env.NODE_ENV !== 'test'){
+    if(process.env.NODE_ENV !== 'test' && process.env.NODE_ENV !== 'jest' ){
         return res.status(400).json({error: 'Access denied. Only allowed in test environment.'});
     }
     try{
         const {name, email, password} = req.query;
-        if(name|| !email || !password){
-
+        if(!name|| !email || !password){
+            throw new Error('Missing Parameters');
         }
         await prisma.user.create({
             data: {
@@ -50,9 +50,3 @@ export const addVerifiedUser = async (req: Request, res: Response) =>{
         res.status(400).json({error: 'Error 400'});
     }
 };
-
-// export const addVerifiedUser = async (req: Request, res: Response) =>{
-//     if(process.env.NODE_ENV !== 'test'){
-//         return res.status(400).json({error: 'Access denied. Only allowed in test environment.'});
-//     }
-// }
