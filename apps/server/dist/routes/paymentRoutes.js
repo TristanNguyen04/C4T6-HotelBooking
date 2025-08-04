22ec4ca9af5ae68a9ad02a6cbb01485200dc72f0
TristanNguyen04 <nquocdung.04@gmail.com>
2025-08-04 17:23:42 +0800

    fixed dependencies

--- apps/server/dist/routes/paymentRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const paymentController_1 = require("../controllers/paymentController");
-const router = (0, express_1.Router)();
-router.post("/payments/checkout", paymentController_1.createCheckoutSession);
-router.post("/payments/success", paymentController_1.handlePaymentSuccess);
-router.post("/payments/mock-success", paymentController_1.mockPaymentSuccess);
-exports.default = router;
