953165618ddc4b99ab60c1863f38b66f0f2b27f6
TristanNguyen04 <nquocdung.04@gmail.com>
2025-08-04 19:28:17 +0800

    fix prisma client

--- apps/server/src/utils/prismaClient.ts
+++ apps/server/src/utils/prismaClient.ts
@@ -1,29 +1,12 @@
 import dotenv from 'dotenv';
 dotenv.config();
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
+console.log('DATABASE_TEST_URL:', process.env.DATABASE_TEST_URL);
 
 import { PrismaClient } from "@prisma/client";
 
 // use the same prisma connection
-const isTestEnv = process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'jest';
-
-// Check if DATABASE_URL exists
-if (!process.env.DATABASE_URL && !isTestEnv) {
-  throw new Error('DATABASE_URL environment variable is not set');
-}
-
-const dbUrl = isTestEnv
-  ? 'postgresql://hotelproject:hotelproject@localhost:5432/db_test'
-  : process.env.DATABASE_URL;
-
-console.log('Prisma is connecting to DB URL:', dbUrl);
-
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: dbUrl
-    },
-  },
-});
+const prisma = new PrismaClient();
 
 export default prisma;
 
